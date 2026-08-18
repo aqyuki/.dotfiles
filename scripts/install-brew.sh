23026bd2a757c6938583c50ed6153c2a17579b8a
aqyuki <yksk171118@gmail.com>
2026-08-18 23:39:19 +0900

    feat(scripts): Homebrewがインストールされていない場合にスクリプトが終了するように変更

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 if ! command -v brew &> /dev/null; then
-  echo "Homebrew is not installed. Install Homebrew first."
-  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -o /tmp/install-homebrew.sh
-  bash /tmp/install-homebrew.sh
-
-  # Add Homebrew to PATH for the current session
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-else
-  echo "Homebrew is already installed."
+  echo "install-brew.sh requires Homebrew to be installed. Please install Homebrew first."
+  exit 1
 fi
 
 # Install tools from Brewfile
