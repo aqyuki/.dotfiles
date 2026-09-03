5c16bd933d57947aa1ccd2245b1c361c2245e002
aqyuki <yksk171118@gmail.com>
2026-09-03 10:01:29 +0900

    feat(config/fish): XDG_BIN_HOMEを追加

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,9 @@ set --global --export XDG_CONFIG_HOME $HOME/.config
 set --global --export XDG_CACHE_HOME $HOME/.cache
 set --global --export XDG_DATA_HOME $HOME/.local/share
 set --global --export XDG_STATE_HOME $HOME/.local/state
+set --global --export XDG_BIN_HOME $HOME/.local/bin
+
+fish_add_path $XDG_BIN_HOME
 
 abbr --add --global lg lazygit
 abbr --add --global proot 'cd (git rev-parse --show-toplevel)'
